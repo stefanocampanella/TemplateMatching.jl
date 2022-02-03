2b3759964b2e565d3d10b02d3c923b05507ab3d1
Stefano Campanella <15182642+stefanocampanella@users.noreply.github.com>
2022-02-03 12:40:21 +0100

    Add crosscorrelation function

--- src/TemplateMatching.jl
+++ src/TemplateMatching.jl
@@ -1,5 +1,43 @@
 module TemplateMatching
 
-# Write your package code here.
+using StatsBase
+using LinearAlgebra
+
+export crosscorrelate
+
+"""
+    crosscorrelate(series, template, cc_eltype=Float64; normalize_template=false) -> Vector{cc_eltype}
+
+    Return the normalized cross-correlation `cc` between `series` and `template`. If `series` is a vector of ``n`` elements 
+    and template a vector of ``k`` elements, then `cc` is a vector of ``n - k + 1`` elements of type `cc_eltype`. 
+    The element of `cc` at position ``l`` is Pearson correlation coefficient between the segment of `series` 
+    starting at ``l`` and ``k`` elements long. See the [Wikipedia page](https://en.wikipedia.org/wiki/Cross-correlation#Normalization)
+    on cross-correlation for more details.
+"""
+function crosscorrelate(series::AbstractVector{T1}, template::AbstractVector{T2}, 
+    cc_eltype::Type{T3}=Float64; normalize_template=true) where {T1 <: Number, T2 <: Number, T3 <: AbstractFloat}
+    
+    if isempty(series) || isempty(template)
+        error("Arguments must be non empty vectors")
+    end
+
+    if normalize_template
+        y_mean, y_std = mean_and_std(template, corrected=false)
+        y_norm = @. (template - y_mean) / y_std
+    else
+        y_norm = template
+    end
+
+    N = size(template, 1)
+    cc = Vector{cc_eltype}(undef, size(series, 1) - N + 1)
+    x_norm = similar(y_norm) 
+    for n in eachindex(cc)
+        x_view = view(series, n:n + N - 1)
+        x_mean, x_std = mean_and_std(x_view, corrected=false)
+        @. x_norm = (x_view - x_mean) / x_std
+        cc[n] = dot(x_norm, y_norm) / N
+    end
+    cc
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,13 @@ using TemplateMatching
 using Test
 
 @testset "TemplateMatching.jl" begin
-    # Write your tests here.
+    @test_throws ErrorException crosscorrelate([1, 2, 3], Int[])
+    @test_throws ErrorException crosscorrelate(Int[], [1, 2, 3])
+    @test length(crosscorrelate([1, 2, 3], [1, 2])) == 2
+    @test let 
+        x = @. 100.0 * ($rand(100) - 0.5)
+        y = @view x[45:55]
+        cc, indx = findmax(crosscorrelate(x, y))
+        cc ≈ 1.0 && indx == 45
+    end
 end
